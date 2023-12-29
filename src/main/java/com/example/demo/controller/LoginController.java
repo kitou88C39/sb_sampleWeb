5e43a98ec76c6110f8a6ea0c58cb5b18f7818ee7
kitou88C39 <kanfutrooper@icloud.com>
2023-12-29 09:38:43 +0900

    Add controller/LoginController.java

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -2,9 +2,15 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    @GetMapping("path")
+    public String view() {
+        return "login";
+    }
 
 }
