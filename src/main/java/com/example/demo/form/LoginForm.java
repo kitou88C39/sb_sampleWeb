8f1ff8ce38cebaddab2910bf147ba2a721049a57
kitou88C39 <kanfutrooper@icloud.com>
2023-12-31 12:29:48 +0900

    Add thymeleaf to login.html

--- src/main/java/com/example/demo/form/LoginForm.java
+++ src/main/java/com/example/demo/form/LoginForm.java
@@ -3,10 +3,9 @@ package com.example.demo.form;
 import lombok.Data;
 
 @Data
-
 public class LoginForm {
     
-private String loginId;
-private String password;
+    private String loginId;
+    private String password;
 
 }
