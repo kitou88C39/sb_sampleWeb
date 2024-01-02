61ca6d1fce95fcbb9dff77c7ab89f9e5b89db5fb
kitou88C39 <kanfutrooper@icloud.com>
2024-01-02 15:16:48 +0900

    Add code to src/main/java/com.example.demo/UserInfo.java

--- src/main/java/com/example/demo/entity/UserInfo.java
+++ src/main/java/com/example/demo/entity/UserInfo.java
@@ -2,9 +2,14 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "user_info")
+@Data
 public class UserInfo {
+
+    private String loginId;
+    private String password;
 
 }
