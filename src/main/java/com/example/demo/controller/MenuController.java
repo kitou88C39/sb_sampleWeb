8e641a272260a473ad0f15b687a2182a3236556c
kitou88C39 <kanfutrooper@icloud.com>
2024-01-02 12:21:20 +0900

    refactoring

--- src/main/java/com/example/demo/controller/MenuController.java
+++ src/main/java/com/example/demo/controller/MenuController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MenuController {
 
-    @getMapping(("/menu"))
+    @GetMapping(("/menu"))
     public String view() {
         return "menu";
     }
