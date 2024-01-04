2f620e7d629f322d9714dd9b54992e31011a299f
kitou88C39 <kanfutrooper@icloud.com>
2024-01-04 09:35:21 +0900

    Create config package and BeanDefine class in src/main/java/com.example.demo

--- src/main/java/com/example/demo/config/BeanDefine.java
+++ src/main/java/com/example/demo/config/BeanDefine.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanDefine {
