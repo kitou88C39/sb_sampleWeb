0c58a8235a8173ef78ab1ec380017bfe892b39c4
kitou88C39 <kanfutrooper@icloud.com>
2024-01-05 08:56:57 +0900

    コミットメッセージ

--- src/main/java/com/example/demo/repository/UserInfoRepository.java
+++ src/main/java/com/example/demo/repository/UserInfoRepository.java
@@ -1,12 +1,10 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.UserInfo;
 
 //User information table Dao
-@Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
 
 }
