29ba9e39bbefccea04f3f8ce49c12e074693981b
kitou88C39 <kanfutrooper@icloud.com>
2024-01-05 11:46:18 +0900

    Create constant package and ErrorMeddageConst class in src/main/java/com.example.demo

--- src/main/java/com/example/demo/constant/ErrorMeddageConst.java
+++ src/main/java/com/example/demo/constant/ErrorMeddageConst.java
@@ -1,5 +1,7 @@
 package com.example.demo.constant;
 
+//Error message ID constant class
 public class ErrorMeddageConst {
-    
+    // Incorrect input on login screen
+    public static final String LOGIN_WRONG_INPUT = "login.wrongInput";
 }
