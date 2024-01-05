f2ba5201d262c931d2b4acf79319d343921406a2
kitou88C39 <kanfutrooper@icloud.com>
2024-01-05 09:28:57 +0900

    Create util package and AppUtil class in src/main/java/com.example.demo

--- src/main/java/com/example/demo/util/AppUtil.java
+++ src/main/java/com/example/demo/util/AppUtil.java
@@ -1,5 +1,10 @@
 package com.example.demo.util;
 
+//Application common class
 public class AppUtil {
-    
+
+    public static String getMessage(MessageSource messageSource, String key, Object...objects prams){
+        
+    }
+
 }
