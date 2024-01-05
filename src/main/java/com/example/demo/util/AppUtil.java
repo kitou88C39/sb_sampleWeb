46e7b1f2bbe3d67d2802db90875251fb1cb0488c
kitou88C39 <kanfutrooper@icloud.com>
2024-01-05 09:33:05 +0900

    Create util package and AppUtil class in src/main/java/com.example.demo

--- src/main/java/com/example/demo/util/AppUtil.java
+++ src/main/java/com/example/demo/util/AppUtil.java
@@ -1,13 +1,14 @@
 package com.example.demo.util;
 
 import org.springframework.cglib.core.Local;
+
 import org.springframework.context.MessageSource;
 
 //Application common class
 public class AppUtil {
 
-    public static String getMessage(MessageSource messageSource, String key, Object...objects prams){
-        return messageSource.getMessage(key,params,Local.JAPAN);
+    public static String getMessage(MessageSource messageSource, String key, Object... params) {
+        return messageSource.getMessage(key, params, Local.JAPAN);
     }
 
 }
