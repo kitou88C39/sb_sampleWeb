869fadb4c55967cfb39ac0ebf5d8427602e65217
kitou88C39 <kanfutrooper@icloud.com>
2024-01-06 15:21:31 +0900

    SignupController file in controller folder

--- src/main/java/com/example/demo/controller/SignupController.java
+++ src/main/java/com/example/demo/controller/SignupController.java
@@ -1,5 +1,56 @@
 package com.example.demo.controller;
 
+import org.springframework.context.MessageSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.form.LoginForm;
+import com.example.demo.service.LoginService;
+import com.example.demo.util.AppUtil;
+
+import lombok.RequiredArgsConstructor;
+
+//LoginScreenController
+@Controller
+@RequiredArgsConstructor
 public class SignupController {
-    
+
+    // LoginService
+    private final LoginService service;
+    // passwordEncoder
+    private final PasswordEncoder passwordEncoder;
+
+    private final MessageSource messageSource;
+
+    // Initial display
+    // @Param modelモデル
+    // @Param form入力情報
+    // return 表示画面
+
+    @GetMapping("/login")
+    public String view(Model model, LoginForm form) {
+        return "login";
+    }
+
+    // Login
+    // @Param modelモデル
+    // @Param form入力情報
+    // return 表示画面
+
+    @PostMapping("/login")
+    public String login(Model model, LoginForm form) {
+        var userInfo = service.searchUserById(form.getLoginId());
+        var isCorrectUserAuth = userInfo.isPresent()
+                && passwordEncoder.matches(form.getPassword(), userInfo.get().getPassword());
+        if (isCorrectUserAuth) {
+            return "redirect:/menu";
+        } else {
+            var errorMsg = AppUtil.getMessage(messageSource, ErrorMessageConst.LOGIN_WRONG_INPUT);
+            model.addAttribute("errorMsq", errorMsg);
+            return "login";
+        }
+    }
 }
