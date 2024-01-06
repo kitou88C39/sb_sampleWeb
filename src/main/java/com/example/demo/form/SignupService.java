bee351acd89ce61ff2feaccefd6412e50440b556
kitou88C39 <kanfutrooper@icloud.com>
2024-01-06 16:21:23 +0900

    Create a SignupService class in service

--- src/main/java/com/example/demo/form/SignupService.java
+++ src/main/java/com/example/demo/form/SignupService.java
@@ -1,5 +1,16 @@
 package com.example.demo.form;
 
+//LoginScreen Service
+
 public class SignupService {
-    
+
+    // LoginId
+    private String loginId;
+    // Password
+    private String password;
+
+    public String getLoginId() {
+        return this.loginId;
+    }
+
 }
