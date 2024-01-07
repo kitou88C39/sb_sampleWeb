fe0bef6be790c60e1d62b72059c2401b90ccf8b2
kitou88C39 <kanfutrooper@icloud.com>
2024-01-07 11:41:25 +0900

    Add Dozar Mapper to project

--- src/main/java/com/example/demo/service/SignupService.java
+++ src/main/java/com/example/demo/service/SignupService.java
@@ -1,6 +1,10 @@
 package com.example.demo.service;
 
 import dozer.DozerBeanMapper;
+
+import org.apache.catalina.mapper.Mapper;
+import org.springframework.context.MessageSource;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.UserInfo;
@@ -16,9 +20,13 @@ public class SignupService {
     private final UserInfoRepository repository;
     // Dozer Mapper
     private final Mapper mapper;
+    // passwordEncoder
+    private final PasswordEncoder passwordEncoder;
 
     public UserInfo resistUserById(SignupForm form) {
         var userInfo = mapper.map(form, UserInfo.class);
+
+        var encodedPassword = passwordEncoder.encode(form.getPassword());
 
         return repository.save(userInfo);
     }
