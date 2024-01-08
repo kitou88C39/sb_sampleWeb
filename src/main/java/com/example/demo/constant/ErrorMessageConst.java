518302d3aaeb6da916e05d7eb26a6d0ad49d7970
kitou88C39 <kanfutrooper@icloud.com>
2024-01-08 17:29:39 +0900

    Edit messages.properties

--- src/main/java/com/example/demo/constant/ErrorMessageConst.java
+++ src/main/java/com/example/demo/constant/ErrorMessageConst.java
@@ -4,6 +4,8 @@ package com.example.demo.constant;
 public class ErrorMessageConst {
     // Incorrect input on login screen
     public static final String LOGIN_WRONG_INPUT = "login.wrongInput";
-    // User registration screen
+    // User registration screen: Already registered login ID
     public static final String SIGNUP_EXISTED_LOGIN_ID = "signup.extendLoginId";
+    // User registration screen:User registration completed
+    public static final String SIGNUP_RESIST_SUCCEED = "signup.resistSucceed";
 }
